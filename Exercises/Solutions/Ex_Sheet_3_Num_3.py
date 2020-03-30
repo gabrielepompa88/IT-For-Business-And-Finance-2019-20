96745ad0521fba1e4a32d8a5d9373c03875a78cd
gabrielepompa88 <gabriele.pompa@gmail.com>
2020-03-30 21:47:18 +0200

    Ex_Sheet_3_Num_3 updated

--- Exercises/Solutions/Ex_Sheet_3_Num_3.py
+++ Exercises/Solutions/Ex_Sheet_3_Num_3.py
@@ -8,26 +8,8 @@ File name: Ex_Sheet_3_Num_3.py
 Description: This script changes the indexing of a given series.
 """
 
-import copy # for copy.deepcopy()
 import pandas as pd
 
-
-def squaredIndex(ser):
-    """
-    Function squaredIndex(ser) returns series 'ser' reindexed according to
-    the square of the elements of its own index
-    
-    Parameters:
-        ser (pandas.Series): series to reindex
-        
-    Returns:
-        ser (pandas.Series): reindexed series
-        
-    """
-    
-    ser.index = [i**2 for i in ser.index]
-    
-    return ser
 
 def main():
     """
@@ -44,14 +26,8 @@ def main():
     # test series
     s = pd.Series(data=[2, 4, 6, 8, 10])
     
-    # calculations : copy.deepopy(s) makes a copy of s to pass it to squaredIndex
-    # This is crucial, otherwise not only s_reindexed will have the new index,
-    # but also s. Try to remove it and just 
-    #
-    # s_reindexed = squaredIndex(s)
-    # 
-    # to see how the printed indexes change...
-    s_reindexed = squaredIndex(copy.deepcopy(s))
+    # renaming of the index
+    s_reindexed = s.rename(index = {i: i**2 for i in s.index})
     
     # Print section ('\n' is to print on the next line)
     print("s: \n{}".format(s))
